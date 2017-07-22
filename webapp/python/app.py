f7c62d4c5757c9d4c03d0039c385465709e814ac
showwin <showwin_kmc@yahoo.co.jp>
2017-07-22 17:45:41 +0900

    add Python Implementation

--- webapp/python/app.py
+++ webapp/python/app.py
@@ -48,6 +48,20 @@ def db():
         return request.db
 
 
+@app.teardown_request
+def close_db(exception=None):
+    if hasattr(request, 'db'):
+        request.db.close()
+
+
+def to_jst(datetime_utc):
+    return datetime_utc + datetime.timedelta(hours=9)
+
+
+def to_utc(datetime_jst):
+    return datetime_jst - datetime.timedelta(hours=9)
+
+
 def authenticate(email, password):
     cur = db().cursor()
     cur.execute("SELECT * FROM users WHERE email = %s", (email, ))
@@ -58,15 +72,67 @@ def authenticate(email, password):
         session['user_id'] = user['id']
 
 
+def authenticated():
+    if not current_user():
+        abort(401)
+
+
+def current_user():
+    if 'user_id' in session:
+        cur = db().cursor()
+        cur.execute('SELECT * FROM users WHERE id = %s', str(session['user_id']))
+        return cur.fetchone()
+    else:
+        return None
+
+
 def update_last_login(user_id):
     cur = db().cursor()
     cur.execute('UPDATE users SET last_login = %s WHERE id = %s', (datetime.datetime.now(), user_id,))
 
 
-def current_user():
+def get_comments(product_id):
     cur = db().cursor()
-    cur.execute('SELECT * FROM users WHERE id = %s', str(session['user_id']))
-    return cur.fetchone()
+    cur.execute("""
+SELECT *
+FROM comments as c
+INNER JOIN users as u
+ON c.user_id = u.id
+WHERE c.product_id = {}
+ORDER BY c.created_at DESC
+LIMIT 5
+""".format(product_id))
+
+    return cur.fetchall()
+
+
+def get_comments_count(product_id):
+    cur = db().cursor()
+    cur.execute('SELECT count(*) as count FROM comments WHERE product_id = {}'.format(product_id))
+    return cur.fetchone()['count']
+
+
+def buy_product(product_id, user_id):
+    cur = db().cursor()
+    cur.execute("INSERT INTO histories (product_id, user_id, created_at) VALUES ({}, {}, \'{}\')".format(
+        product_id, user_id, to_utc(datetime.datetime.now()).strftime('%Y-%m-%d %H:%M:%S')))
+
+
+def already_bought(product_id):
+    if not current_user():
+        return False
+    cur = db().cursor()
+    cur.execute('SELECT count(*) as count FROM histories WHERE product_id = %s AND user_id = %s',
+                (product_id, current_user()['id']))
+    return cur.fetchone()['count'] > 0
+
+
+def create_comment(product_id, user_id, content):
+    cur = db().cursor()
+    cur.execute("""
+INSERT INTO comments (product_id, user_id, content, created_at)
+VALUES ({}, {}, '{}', '{}')
+""".format(product_id, user_id, content, to_utc(datetime.datetime.now()).strftime('%Y-%m-%d %H:%M:%S')))
 
 
 @app.errorhandler(401)
@@ -74,16 +140,15 @@ def authentication_error(error):
     return render_template('login.html', message='ログインに失敗しました'), 401
 
 
-@app.teardown_request
-def close_db(exception=None):
-    if hasattr(request, 'db'):
-        request.db.close()
+@app.errorhandler(403)
+def authentication_error(error):
+    return render_template('login.html', message='先にログインをしてください'), 403
 
 
 @app.route('/login')
 def get_login():
     session.pop('user_id', None)
-    return render_template('login.html', messae='ECサイトで爆買いしよう！！！！')
+    return render_template('login.html', message='ECサイトで爆買いしよう！！！！')
 
 
 @app.route('/login', methods=['POST'])
@@ -91,6 +156,96 @@ def post_login():
     authenticate(request.form['email'], request.form['password'])
     update_last_login(current_user()['id'])
     return redirect('/')
+
+
+@app.route('/logout')
+def get_logout():
+    session.pop('user_id', None)
+    return redirect('/login')
+
+
+@app.route('/')
+def get_index():
+    page = int(request.args.get('page', 0))
+    cur = db().cursor()
+    cur.execute("SELECT * FROM products ORDER BY id DESC LIMIT 50 OFFSET {}".format(page * 50))
+    products = cur.fetchall()
+
+    for product in products:
+        product['description'] = product['description'][:70]
+        product['created_at'] = to_jst(product['created_at'])
+        product['comments'] = get_comments(product['id'])
+        product['comments_count'] = get_comments_count(product['id'])
+
+    return render_template('index.html', products=products, current_user=current_user())
+
+
+@app.route('/users/<int:user_id>')
+def get_mypage(user_id):
+    cur = db().cursor()
+    cur.execute("""
+SELECT p.id, p.name, p.description, p.image_path, p.price, h.created_at
+FROM histories as h
+LEFT OUTER JOIN products as p
+ON h.product_id = p.id
+WHERE h.user_id = {}
+ORDER BY h.id DESC
+""".format(str(user_id)))
+
+    products = cur.fetchall()
+
+    total_pay = 0
+    for product in products:
+        total_pay += product['price']
+        product['description'] = product['description'][:70]
+        product['created_at'] = to_jst(product['created_at'])
+
+    cur = db().cursor()
+    cur.execute('SELECT * FROM users WHERE id = {}'.format(str(user_id)))
+    user = cur.fetchone()
+
+    return render_template('mypage.html', products=products, user=user,
+                                          total_pay=total_pay, current_user=current_user())
+
+
+@app.route('/products/<int:product_id>')
+def get_product(product_id):
+    cur = db().cursor()
+    cur.execute('SELECT * FROM products WHERE id = {}'.format(product_id))
+    product = cur.fetchone()
+
+    cur = db().cursor()
+    cur.execute('SELECT * FROM comments WHERE product_id = {}'.format(product_id))
+    comments = cur.fetchall()
+
+    return render_template('product.html',
+                           product=product, comments=comments, current_user=current_user(),
+                           already_bought=already_bought(product_id))
+
+
+@app.route('/products/buy/<int:product_id>', methods=['POST'])
+def post_products_buy(product_id):
+    authenticated()
+    buy_product(product_id, current_user()['id'])
+
+    return redirect("/users/{}".format(current_user()['id']))
+
+
+@app.route('/comments/<int:product_id>', methods=['POST'])
+def post_comments(product_id):
+    authenticated()
+    create_comment(product_id, current_user()['id'], request.form['content'])
+    return redirect("/users/{}".format(current_user()['id']))
+
+
+@app.route('/initialize')
+def get_initialize():
+    cur = db().cursor()
+    cur.execute('DELETE FROM users WHERE id > 5000')
+    cur.execute('DELETE FROM products WHERE id > 10000')
+    cur.execute('DELETE FROM comments WHERE id > 200000')
+    cur.execute('DELETE FROM histories WHERE id > 500000')
+    return ("Finish")
 
 
 if __name__ == "__main__":
