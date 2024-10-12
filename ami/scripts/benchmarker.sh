8d91d6f43287466dfc290e15978c133ae3f8ea37
showwin <showwin_kmc@yahoo.co.jp>
2024-10-12 12:33:33 +0900

    Benchmarkerはアプリケーションとは別のDBを参照することでアプリケーション改善による影響を受けないように修正

--- ami/scripts/benchmarker.sh
+++ ami/scripts/benchmarker.sh
@@ -7,3 +7,9 @@ cd /tmp/
 tar -zxvf /tmp/benchmarker.tar.gz
 cd /tmp/benchmarker
 go build -x -o /home/ishocon/benchmark *.go
+
+# Load data into MySQL
+sudo mysql -u root -pishocon1 -e 'CREATE DATABASE IF NOT EXISTS ishocon1_bench;'
+sudo mysql -u root -pishocon1 -e "CREATE USER IF NOT EXISTS ishocon IDENTIFIED BY 'ishocon';"
+sudo mysql -u root -pishocon1 -e 'GRANT ALL ON *.* TO ishocon;'
+tar -zxvf ~/data/ishocon1.dump.tar.gz -C ~/data && sudo mysql -u root -pishocon1 ishocon1_bench < ~/data/ishocon1.dump
